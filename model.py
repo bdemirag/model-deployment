da6edc6312ffe738203230708af251f1c4472a12
bdemirag <bdemirag@hotmail.com>
2024-02-21 23:22:18 +0300

    Added insurance.csv

--- model.py
+++ model.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
@@ -8,4 +7,35 @@ import pickle
 
 # Load the csv file
 df = pd.read_csv("insurance.csv")
+print(df.head(10))
+#print(df.info)
+
+label_encoder = preprocessing.LabelEncoder()
+df['sex']= label_encoder.fit_transform(df['sex'])
+df['smoker']= label_encoder.fit_transform(df['smoker'])
+df['region']= label_encoder.fit_transform(df['region'])
+
+df['sex'].unique()
+df['smoker'].unique()
+df['region'].unique()
+
+X = df[["sex", "bmi", "children", "region", "charges", "age"]]
+y = df["smoker"]
+
 print(df.head())
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=50)
+
+# Feature scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test= sc.transform(X_test)
+
+# Instantiate the model
+classifier = RandomForestClassifier()
+
+# Fit the model
+classifier.fit(X_train, y_train)
+
+# Make pickle file of our model
+pickle.dump(classifier, open("model.pkl", "wb"))
